af3d7137cefb4456b66282cbbeea7862c5654dbe
Jan-Fleming Nickelsen <jan-fleming.nickelsen@hmmh.de>
2022-09-07 10:42:09 +0200

    added nonfunctional color picker and fixed window resizing

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
+import GUI from './node_modules/lil-gui/dist/lil-gui.esm.js';
+
+const gui = new GUI();
+
+const myObject = {
+	myBoolean: true,
+	//myFunction: function() { ... },
+	myString: 'lil-gui',
+	myNumber: 1
+};
+
+// gui.add( myObject, 'myBoolean' );  // Checkbox
+// //gui.add( myObject, 'myFunction' ); // Button
+// gui.add( myObject, 'myString' );   // Text Field
+// gui.add( myObject, 'myNumber' );   // Number Field
+
+// // Add sliders to number fields by passing min and max
+// gui.add( myObject, 'myNumber', 0, 1 );
+// gui.add( myObject, 'myNumber', 0, 100, 2 ); // snap to even numbers
+
+// // Create dropdowns by passing an array or object of named values
+// gui.add( myObject, 'myNumber', [ 0, 1, 2 ] );
+// gui.add( myObject, 'myNumber', { Label1: 0, Label2: 1, Label3: 2 } );
+
+// // Chainable methods
+// gui.add( myObject, 'myProperty' )
+// 	.name( 'Custom Name' )
+// 	.onChange( value => {
+// 		console.log( value );
+// 	} );
+
+// Create color pickers for multiple color formats
+const colorFormats = {
+	Color: '#ffffff',
+	int: 0xffffff,
+	object: { r: 1, g: 1, b: 1 },
+	array: [ 1, 1, 1 ],
+};
+
+gui.addColor( colorFormats, 'Color' );
+
 //Variables for Setup
 
 let container;
@@ -58,6 +99,18 @@ function init(){
         renderer.render(scene, camera);
     }
 
+    window.addEventListener( 'resize', onWindowResize );
+
 }
 
 init();
+
+function onWindowResize() {
+
+    camera.aspect = (container.clientWidth/2)/(container.clientHeight/2);
+    camera.updateProjectionMatrix();
+    let w = container.offsetWidth;
+    let h = container.offsetHeight;
+    renderer.setSize( w, h );
+
+}
